fbcb43d4341ce215e05abbac3995a57897ed083c
fasalpa <psfr97@gmail.com>
2026-04-08 15:20:26 -0500

    agregando descripciones del tarot tóxico y estilos

---
+++ js/app.js
@@ -0,0 +1,14 @@
+<<<<<<< Updated upstream
+=======
+let boton = document.getElementById("btn-enviar")
+let result = document.getElementById("formularioEnviado")
+
+boton.addEventListener("click", () => {
+  let alerta = result
+  alerta.textContent = "Solicitud enviada con éxito"
+})
+
+function voltear(card) {
+  card.classList.toggle('flipped');
+}
+>>>>>>> Stashed changes
